d575d20e08e0485f3f9a2d1d36f1902c0de7f378
llanas <12050570+llanas@users.noreply.github.com>
2019-11-23 16:23:50 +0100

    Simple websocket implementation

--- server/server.ts
+++ server/server.ts
@@ -4,30 +4,35 @@ import { Collection } from '@shared/collection';
 import { SocketClient } from '@shared/core/socket-client';
 import * as express from 'express';
 import * as http from 'http';
+import * as WebSocket from 'ws';
 
 import { ServerSocketClient } from './core/server-socker-client';
 
 class Server {
 
-    private static instance: Server;
-
     app: any;
     server: http.Server;
+    socket: WebSocket.Server;
     clients: Collection<SocketClient>;
 
     constructor(port: number) {
 
         this.app = express();
         this.server = new http.Server(this.app);
-        this.server.listen(port);
-
+        this.socket = new WebSocket.Server({ port });
+        console.log('Listening on port : ' + port);
+        this.socket.on('connection', this.newConnection);
         this.clients = new Collection<ServerSocketClient>([], 'id', true);
-        console.log('listening on port : %s', port);
     }
 
-    static build(port: number) {
-        Server.instance = new Server(port);
+    newConnection(ws: WebSocket, request: http.IncomingMessage) {
+        console.log('Nouvelle connexion');
+        ws.on('message', (message: string) => {
+            console.log('received: %s', message);
+            ws.send(`hello, you sent -> ${message}`);
+        });
+        ws.send('Hi there, I am a WebSocket server');
     }
 }
 
-Server.build(8080);
+new Server(8080);
